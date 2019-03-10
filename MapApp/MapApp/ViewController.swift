ddf0723d8ac13b33e87146806f67b32ebb5cd391
yuta sasaki <rain.00.00.09@gmail.com>
2019-03-10 18:24:34 +0900

    Watch側に距離情報の転送

--- MapApp/MapApp/ViewController.swift
+++ MapApp/MapApp/ViewController.swift
@@ -9,21 +9,31 @@
 import UIKit
 import MapKit
 import CoreLocation
+import WatchConnectivity //saski
 
 class ViewController:   UIViewController,
                         CLLocationManagerDelegate,
-                        UIGestureRecognizerDelegate {
+                        UIGestureRecognizerDelegate,
+                        WCSessionDelegate { //sasaki
 
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var longPressGesRec: UILongPressGestureRecognizer!
     var locManager: CLLocationManager!
     var pointAno: MKPointAnnotation = MKPointAnnotation()
     var mapViewType: UIButton!
+    var session = WCSession.default; //saski
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        // sasaki
+        if (WCSession.isSupported()) {
+            self.session = WCSession.default
+            self.session.delegate = self
+            self.session.activate()
+        }
+        
         // CLLocationManagerのdelegateを登録する
         locManager = CLLocationManager()
         locManager.delegate = self
@@ -45,6 +55,23 @@ class ViewController:   UIViewController,
         initMap()
     }
     
+    //sasaki start
+    public func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Swift.Void){
+        print("receiveMessage::\(message)")
+    }
+    
+    public func session(_ session: WCSession, didFinish userInfoTransfer: WCSessionUserInfoTransfer, error: Error?) {
+        print("userInfoTransfer::\(userInfoTransfer)")
+    }
+    public func session(_ session: WCSession, activationDidCompleteWith activationState:    WCSessionActivationState, error: Error?){
+    }
+    public func sessionDidBecomeInactive(_ session: WCSession){
+    }
+    public func sessionDidDeactivate(_ session: WCSession){
+    }
+    //sasaki end
+        
+        
     // 地図の初期化
     func initMap() {
         // 縮尺を設定
@@ -64,28 +91,32 @@ class ViewController:   UIViewController,
         
         // 地図表示タイプを切り替えるボタン
         mapViewType = UIButton(type: UIButton.ButtonType.detailDisclosure)
-        mapViewType.frame = CGRect(x:width - 50, y:60, width:40, height:40)
+        mapViewType.frame = CGRect(x:width - 50, y:58, width:40, height:40)
         mapViewType.layer.backgroundColor = UIColor(white: 1, alpha: 0.8).cgColor // 背景色
-        mapViewType.layer.borderWidth = 0.5 // 枠線の幅
-        mapViewType.layer.borderColor = UIColor.blue.cgColor // 枠線の色
-//      mapViewType.layer.cornerRadius = 10.0 // 角丸のサイズ
-//      mapViewType.layer.shadowRadius = 5 // ぼかし
+//      mapViewType.layer.borderWidth = 0.5 // 枠線の幅
+//      mapViewType.layer.borderColor = UIColor.blue.cgColor // 枠線の色
+        mapViewType.layer.masksToBounds = false
+        mapViewType.layer.shadowColor = UIColor.black.cgColor
+        mapViewType.layer.shadowOffset = CGSize(width: 2, height: 2)
+        mapViewType.layer.shadowOpacity = 0.2
+        mapViewType.layer.shadowRadius = 3 // ぼかし
+        mapViewType.layer.cornerRadius = 5.0 // 角丸のサイズ
         self.view.addSubview(mapViewType)
         mapViewType.addTarget(self, action: #selector(ViewController.mapViewTypeBtnThouchDown(_:)), for: .touchDown)
+        
         // トラッキングボタン表示
         let trakingBtn = MKUserTrackingButton(mapView: mapView)
         trakingBtn.layer.backgroundColor = UIColor(white: 1, alpha: 0.8).cgColor
         trakingBtn.frame = CGRect(x:width - 50, y:100, width:40, height:40)
-        trakingBtn.layer.borderWidth = 0.5 // 枠線の幅
-        trakingBtn.layer.borderColor = UIColor.blue.cgColor // 枠線の色
+//      trakingBtn.layer.borderWidth = 0.5 // 枠線の幅
+//      trakingBtn.layer.borderColor = UIColor.blue.cgColor // 枠線の色
+        trakingBtn.layer.shadowColor = UIColor.black.cgColor
+        trakingBtn.layer.shadowColor = UIColor.black.cgColor
+        trakingBtn.layer.shadowOffset = CGSize(width: 2, height: 2)
+        trakingBtn.layer.shadowOpacity = 0.2
+        trakingBtn.layer.shadowRadius = 3 // ぼかし
+        trakingBtn.layer.cornerRadius = 5.0 // 角丸のサイズ
         self.view.addSubview(trakingBtn)
-        
-        // スケールバーの表示
-        let scale = MKScaleView(mapView: mapView)
-        scale.frame.origin.x = 15
-        scale.frame.origin.y = 45
-        scale.legendAlignment = .leading
-        self.view.addSubview(scale)
         
         // コンパスの表示
         let compass = MKCompassButton(mapView: mapView)
@@ -94,6 +125,13 @@ class ViewController:   UIViewController,
         self.view.addSubview(compass)
         // デフォルトのコンパスを非表示にする
         mapView.showsCompass = false
+
+        // スケールバーの表示
+        let scale = MKScaleView(mapView: mapView)
+        scale.frame.origin.x = 15
+        scale.frame.origin.y = 45
+        scale.legendAlignment = .leading
+        self.view.addSubview(scale)
     }
 
     // CLLocationManagerのdelegate：現在位置取得
@@ -134,7 +172,7 @@ class ViewController:   UIViewController,
             
             // yard
             let yardStr = Int(distance * 1.09361)
-            str = str + " / " + yardStr.description + " yard"
+            str = str + " / " + yardStr.description + " y"
             
             if pointAno.title != str {
                 // ピンまでの距離に変化があればtitleを更新する
@@ -179,12 +217,20 @@ class ViewController:   UIViewController,
 
             // yard
             let yardStr = Int(distance * 1.09361)
-            str = str + " / " + yardStr.description + " yard"
+            str = str + " / " + yardStr.description + " y"
             
             // ロングタップを検出した位置にピンを立てる
             pointAno.coordinate = center
             pointAno.title = str
             mapView.addAnnotation(pointAno)
+            
+            //sasaki
+            let contents =  ["body":str]
+            self.session.sendMessage(contents, replyHandler: { (replyMessage) -> Void in
+                print ("receive from apple watch","test");
+            }) { (error) -> Void in
+                print(error)
+            }
         }
     }
     
@@ -242,4 +288,17 @@ class ViewController:   UIViewController,
         }
     }
     
+}
+
+// MKMapViewDelegate
+extension ViewController : MKMapViewDelegate {
+    // 読み込み開始
+    func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
+        print("map load start")
+    }
+    
+    // 読み込み終了時
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        print("map load ended")
+    }
 }
--- MapApp/MapAppWatch Extension/InterfaceController.swift
+++ MapApp/MapAppWatch Extension/InterfaceController.swift
@@ -8,14 +8,24 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity //sasaki
 
 
-class InterfaceController: WKInterfaceController {
-
+class InterfaceController:  WKInterfaceController,
+                            WCSessionDelegate //sasaki
+{
+    var session = WCSession.default
+    @IBOutlet weak var label: WKInterfaceLabel! //sasaki
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        if (WCSession.isSupported()) {
+            self.session = WCSession.default
+            self.session.delegate = self
+            self.session.activate()
+        }
     }
     
     override func willActivate() {
@@ -28,4 +38,33 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    //sasaki start
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    }
+    
+    public func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Swift.Void){
+        print("receiveMessage::\(message)")
+        
+        guard let kind = message["body"] as? String else {
+            return
+        }
+        
+        label.setText(kind)
+    }
+    public func session(_ session: WCSession, didReceiveMessageData messageData: Data, replyHandler: @escaping (Data) -> Void) {
+        print("receiveMessageData::\(messageData)")
+    }
+    public func session(_ session: WCSession, didFinish userInfoTransfer: WCSessionUserInfoTransfer, error: Error?) {
+        print("userInfoTransfer::\(userInfoTransfer)")
+        
+    }
+    public func session(_ session: WCSession, didReceiveUserInfo userInfo: [String : Any] = [:]){
+        print("didReceiveUserInfo::\(userInfo)");
+    }
+    
+    public func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any])
+    {
+        print("didReceiveApplicationContext::\(applicationContext)");
+    }
+    //sasaki end
 }
