c9d22de59b22358a43504665f54aeca71d066591
sasaki-yuta <42306451+sasaki-yuta@users.noreply.github.com>
2026-06-13 15:06:33 +0900

    地図機能にスクロール機能を追加

--- TASTingNote/TASTingNote/map/MapTabView.swift
+++ TASTingNote/TASTingNote/map/MapTabView.swift
@@ -5,48 +5,88 @@
 //  Created by 佐々木 勇太 on 2026/05/10.
 //
 
+
 import SwiftUI
 import MapKit
 import CoreLocation
-internal import Combine
+import Combine
 
 struct MapTabView: View {
-    
+
     @StateObject private var locationManager = LocationManager()
 
     var body: some View {
-        NavigationView {
-            Map(position: $locationManager.cameraPosition)
-                .ignoresSafeArea(edges: .bottom)
-                .navigationTitle("Map")
+
+        NavigationStack {
+
+            Map(position: $locationManager.cameraPosition) {
+
+                // 現在地を表示
+                UserAnnotation()
+            }
+            .ignoresSafeArea(edges: .bottom)
+            .navigationTitle("Map")
+
+            .overlay(alignment: .bottomTrailing) {
+
+                Button {
+
+                    locationManager.moveToCurrentLocation()
+
+                } label: {
+
+                    Image(systemName: "location.fill")
+                        .font(.title2)
+                        .foregroundStyle(.blue)
+                        .padding()
+                        .background(.ultraThinMaterial)
+                        .clipShape(Circle())
+                        .shadow(radius: 3)
+                }
+                .padding()
+            }
         }
     }
 }
 
-final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    
+final class LocationManager:
+    NSObject,
+    ObservableObject,
+    CLLocationManagerDelegate {
+
     private let manager = CLLocationManager()
-    @Published var cameraPosition: MapCameraPosition = .automatic
-    
+
+    @Published var cameraPosition:
+        MapCameraPosition = .automatic
+
+    @Published var currentLocation:
+        CLLocation?
+
+    // 初回だけ現在地へ移動するためのフラグ
+    private var hasCenteredMap = false
+
     override init() {
+
         super.init()
-        
+
         manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        
-        // 位置情報許可
+
+        manager.desiredAccuracy =
+            kCLLocationAccuracyBest
+
         manager.requestWhenInUseAuthorization()
-        
-        // GPS開始
+
         manager.startUpdatingLocation()
     }
-    
-    func locationManager(
-        _ manager: CLLocationManager,
-        didUpdateLocations locations: [CLLocation]
-    ) {
-        guard let location = locations.first else { return }
-        
+
+    // 現在地ボタン用
+    func moveToCurrentLocation() {
+
+        guard let location =
+            currentLocation else {
+            return
+        }
+
         let region = MKCoordinateRegion(
             center: location.coordinate,
             span: MKCoordinateSpan(
@@ -54,7 +94,67 @@ final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelega
                 longitudeDelta: 0.01
             )
         )
-        
+
         cameraPosition = .region(region)
+    }
+
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+
+        guard let location =
+            locations.first else {
+            return
+        }
+
+        currentLocation = location
+
+        // 初回のみ現在地へ移動
+        guard !hasCenteredMap else {
+            return
+        }
+
+        hasCenteredMap = true
+
+        let region = MKCoordinateRegion(
+            center: location.coordinate,
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            )
+        )
+
+        cameraPosition = .region(region)
+
+        // 以降は更新停止
+        manager.stopUpdatingLocation()
+    }
+
+    func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: Error
+    ) {
+
+        print(
+            "位置情報取得エラー:",
+            error.localizedDescription
+        )
+    }
+
+    func locationManagerDidChangeAuthorization(
+        _ manager: CLLocationManager
+    ) {
+
+        switch manager.authorizationStatus {
+
+        case .authorizedAlways,
+             .authorizedWhenInUse:
+
+            manager.startUpdatingLocation()
+
+        default:
+            break
+        }
     }
 }
